c94f33b009114c64bfa425edf1e3b6b5060a33fb
Christopher Dorst <chris@dorst.email>
2017-12-28 16:29:43 -0600

    Add GitPushCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitPushCommand/GitPushCommandRunner.cs
@@ -0,0 +1,14 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitPushCommand
+{
+    public static class GitPushCommandRunner
+    {
+        private const string push = nameof(push);
+
+        public static void Push(string directory, params string[] arguments)
+            => Git(directory,
+                command: push,
+                arguments);
+    }
+}
